b1b75bbdd03173d666e97e447f8ab85c17154bc6
TonyJHC <jongvin01@naver.com>
2021-11-12 15:44:50 +0900

    [PGS] 타겟넘버 / DFS/BFS / https://programmers.co.kr/learn/courses/30/lessons/43165
    https://programmers.co.kr/learn/courses/30/lessons/42586

--- tony/src/main/java/com/example/tony/Level2/TargetNumber.java
+++ tony/src/main/java/com/example/tony/Level2/TargetNumber.java
@@ -2,9 +2,8 @@ package com.example.tony.Level2;
 
 
 import java.util.LinkedList;
-import java.util.*;
 
-
+/*
 class Graph {
     class Node {
         int data;
@@ -30,75 +29,79 @@ class Graph {
     void addEdge(int i1, int i2) {// 두 노드의 관계를 저장하는 함수
         Node n1 = nodes[i1];
         Node n2 = nodes[i2];
-        if (!n1.adjacent.contains(n2)) {
+//        if (!n1.adjacent.contains(n2)) {
             n1.adjacent.add(n2);
-        }
-        if (!n2.adjacent.contains(n1)) {
+//        }
+//        if (!n2.adjacent.contains(n1)) {
             n2.adjacent.add(n1);
-        }
+//        }
     }
+    // DFS : Recursive
+    void dfsR(Node r){
 
-    void dfs() {
-        dfs(0); // 인자 없으면 시작 노드부터
-    }
-
-    // DFS : Stack
-    void dfs(int index) { // 시작노드 인자로 받아서 순회 결과 출력하는 함수
-        Node root = nodes[index]; // 인덱스로 노드 가져오기
-        Stack<Node> stack = new Stack<>();
-        stack.push(root);
-        root.marked = true; // 방문했음을 표시
-        while(!stack.empty()){
-            Node r = stack.pop() ; // 스택에서 데이터 pop
-            for (Node n : r.adjacent){
-                if(n.marked == false){ // stack에 추가되지 않은 노드들만 추가
-                    n.marked =true; // 방문 표시
-                    stack.push(n);
-                }
+        if(r == null) return;
+        r.marked = true;
+        visit(r);
+        for(Node n : r.adjacent){
+            if(n.marked == false){
+                dfsR(n);
             }
-            visit(r); // 출력
         }
     }
 
-    void bfs(){
-        bfs(0); // 인자 없으면 시작 노드부터
-    }
-
-    // BFS : Queue
-    void bfs(int index){
-        Node root = nodes[index];
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(root); // 시작할 노드 큐에 추가
-        root.marked = true; // 방문
-
-        while(!queue.isEmpty()){
-            Node r = queue.poll(); // 꺼내기
-            for(Node n : r.adjacent){ // 인접한 노드(자식)로 이동 : 인접(자식) 노드 queue에 추가하기 위해
-                if(n.marked == false){ // 방문한적 없다면
-                    n.marked = true; // 방문 했음을 알림
-                    queue.offer(n); // 큐에 추가
-                }
-            }
-            visit(r); // 큐에서 poll한 노드 출력
-        }
+    // DFS : Recursive : array
+    void dfsR(int index){
+        Node r = nodes[index];
+        dfsR(r);
 
     }
 
+    void dfsR(){
+        dfsR(0);
+    }
     void visit(Node n){ // 출력함수
         System.out.print(n.data + " ");
     }
 
-
 }
+*/
+
 public class TargetNumber {
 
+    int count;
 
     public int solution(int[] numbers, int target) {
+        int count = 0;
         int answer = 0;
+        dfs(numbers, 0, target, 0);
+        answer = this.count;
+
         return answer;
+
+    }
+
+    public void dfs(int[] numbers, int depth, int target, int result) {
+        if (depth == numbers.length) {
+            if (target == result) {
+                this.count++;
+            }
+            return;
+        }
+
+        int add = result + numbers[depth];
+        int sub = result - numbers[depth];
+
+        // 재귀 호출시 함수 stack에 의해 역순으로 호출되면서 계산
+        dfs(numbers, depth + 1, target, add);
+        dfs(numbers, depth + 1, target, sub);
+
     }
 
     public static void main(String[] args) {
+        TargetNumber targetNumber = new TargetNumber();
+        int[] numbers = {1, 1, 1, 1, 1};
 
+        int result = targetNumber.solution(numbers, 3);
+        System.out.println("result : " + result);
     }
 }
---
+++ tony/src/main/java/com/example/tony/Level2/TargetNumber2.java
@@ -0,0 +1,13 @@
+package com.example.tony.Level2;
+
+public class TargetNumber2 {
+
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
